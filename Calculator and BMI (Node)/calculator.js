410999cdac59e230f6298d71b625760414bf105f
Gurdarshan Singh <79242952+Gurdarshan-Singh@users.noreply.github.com>
2021-11-10 19:50:39 +0530

    Add files via upload

---
+++ Calculator and BMI (Node)/calculator.js
@@ -0,0 +1,31 @@
+const express =require("express");
+const app =express();
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extented:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname+"/index.html");
+});
+
+app.post("/",function(req,res){
+  var n1=Number(req.body.num1);
+  var n2=Number(req.body.num2);
+  var result =n1+n2;
+  res.send("The result of the calculation is ="+result);
+});
+
+app.listen(3000,function(){
+  console.log("Server is Running on port 3000");
+});
+
+//BMI Calculator
+app.get("/bmicalculator",function(req,res){
+  res.sendFile(__dirname+"/bmiCalculator.html");
+});
+
+app.post("/bmicalculator",function(req,res){
+  var w=parseFloat(req.body.weight);
+  var h=parseFloat(req.body.height);
+  bmi=w/Math.pow(h,2);
+  res.send("Your BMI is "+bmi);
+});
